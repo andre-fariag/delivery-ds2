d6e1554a4a1f743944470504b3eebad4a2f5fa44
Andre Faria <andrefaria-1@hotmail.com>
2021-01-10 18:20:25 -0300

    GET /orders

--- backend/src/main/java/com/delivery/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/delivery/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,17 @@
 package com.delivery.dsdeliver.repositories;
 
+import java.util.List;
+
 import com.delivery.dsdeliver.entities.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository <Order, Long> {
-    
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    // returning only pending orders ordered by moment
+    @Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
+            + " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+    List<Order> findOrdersWithProducts();
+
 }
